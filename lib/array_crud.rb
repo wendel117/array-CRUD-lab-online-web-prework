09dc61fbd9ab6ce9e1076023ed8a7bd2eb6fd5f7
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-10 19:21:22 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  my_array = []
 end
 
 def create_an_array
-  
+candy = ["skittles", "snickers", "reeses", "milkyway"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+array[-1]
 end
